3d04e4f95a51646d6061f7d65469980fe7d0ec10
Kubachoo <koichiseto61@gmail.com>
2025-05-20 18:40:23 -0400

    Started scaling function within dataloader file

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ def main():
     # Data is in the format: ticker,start date(YYYY-MM-DD),end(YYYY-MM-DD)
     stockData = get_stock_data('AMZN','2019-05-14','2024-05-14')
     filepath = Path('/Users/kubacho/workspace/projects/LSTM/AMZN-LSTM/data/historical_data.csv')
+
     # Sends data as CSV file to desired path
     stockData.to_csv(filepath)
-
+    # Prints basic information on CSV file
     print(stockData.head())
     print(stockData.info())
     print(stockData.describe())
-
+    
+    # Visualizes CSV file in order to spot outliers
     visualizeData(filepath)
 
 if __name__ == "__main__":
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,16 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from sklearn import preprocessing
 
-
+# Downloads the historical data of a given stock ticker within it's desired range
 def get_stock_data(ticker, start, end):
     # This downloads the historical data of the stock
     df = yf.download(ticker, start=start, end=end) 
     return df
+
+# Preprocess' and scales pricing for LSTM model
+def price_scaler():
+    
+
 
