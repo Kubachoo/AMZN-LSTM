59c8cfbf82a1b5a7a37a4dcfa17c99960da4feb6
Kubachoo <koichiseto61@gmail.com>
2025-06-30 18:16:46 -0400

    Added .gitignore file and moved code from model.py to main.py to make things easier

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from src.data_loader import get_stock_data
 from src.util import price_scaler, create_sequences 
 from pathlib import Path
 from src.visualize import visualizeData
-from src.model import train_model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+import matplotlib.pyplot as plt
 
 def main():
     # Data is in the format: ticker,start date(YYYY-MM-DD),end(YYYY-MM-DD)
@@ -23,8 +25,25 @@ def main():
     # This function creates the time sequences used for forecasting    
     X_train, y_train = create_sequences(scaled_data,50)
     X_test, y_test = create_sequences(scaled_data,50)
-    train_model()     
+
+
+    # Model training   
+    model = Sequential([
+        LSTM(60,activation='relu', input_shape=(50,1)),
+        Dense(1)
+    ]) 
+    model.compile(optimizer='adam',loss='mse')
+    model.fit(X_train, y_train, epochs=20,batch_size=32,verbose=1)
+
+    train_predictions = model.predict(X_train)
+    test_predictions = model.predict(X_test)
+
+    train_predictions = scaler.inverse_transform(train_predictions)
+    test_predictions = scaler.inverse_transform(test_predictions)
     
+    plt.figure(figsize=(10,6))
+    
+
 if __name__ == "__main__":
     main()
  
--- src/model.py
+++ src/model.py
@@ -1,9 +1,8 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-from main.py import X_train,y_train
 import matplotlib.pyplot as plt
 
-def train_model():
+# def train_model():
     model = Sequential([
         LSTM(60,activation='relu', input_shape=(window_size,1)),
         Dense(1)
